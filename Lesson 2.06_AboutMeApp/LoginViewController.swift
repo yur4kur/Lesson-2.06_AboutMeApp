2a88ea57e8c2d2914a1890bf95d3b070af0512ef
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-27 14:57:05 +0400

    Keyboard logic and alert logic competed.

--- Lesson 2.06_AboutMeApp/LoginViewController.swift
+++ Lesson 2.06_AboutMeApp/LoginViewController.swift
@@ -13,28 +13,48 @@ class LoginViewController: UIViewController {
     @IBOutlet var loginTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
+    // MARK: Private properties
+    private var login = "Jane"
+    private var password = "Sensei"
+    
     // MARK: Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        //textFieldShouldClear(loginTextfield)
-        //textFieldShouldClear(passwordTextfield)
+        loginTextfield.delegate = self
+        passwordTextfield.delegate = self
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(false)
-    }
+    
+    
     
     // MARK: IBActions
-    @IBAction func forgotLoginButton() {
+    @IBAction func forgotLoginButtonDidTap() {
+        showAlert(title: "Ooops!", message: "Your login is '\(login)'")
     }
     
-    @IBAction func forgotPasswordButton() {
+    @IBAction func forgotPasswordButtonDidTap() {
+        showAlert(title: "Ooops!", message: "Your password is '\(password)'")
     }
 }
 
+// MARK: Extensions
 extension LoginViewController: UITextFieldDelegate {
-    internal func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        true
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(false)
+    }
+}
+
+extension LoginViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
